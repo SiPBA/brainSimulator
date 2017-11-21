96a5990582ac9b17fea840abad46c3d0756bd6c8
Francisco J. Martínez <pakitochus@gmail.com>
2017-11-21 12:14:20 +0100

    Getting ready for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('README') as file:
+    long_description = file.read()
+
 setup(
     name="brainSimulator",
-    version="0.5.1",
+    version="0.5.4",
     description="Nuclear brain imaging synthesis with python",
+    long_description=long_description,
     author="SIPBA@UGR",
     author_email="sipba@ugr.es",
     url='https://github.com/SiPBA/brainSimulator',
@@ -24,16 +28,19 @@ setup(
         'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: OS Independent',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL-3.0+',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3', 
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     # ...
 )
